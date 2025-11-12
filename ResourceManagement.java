b9e93b7ff1b0ab239419a1686d1db95bc3d550a5
Wamifawn <garrison.lazard@my.utsa.edu>
2025-11-12 16:42:46 -0600

    Create ResourceManagement.java

---
+++ ResourceManagement.java
@@ -0,0 +1,18 @@
+package src;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.util.Scanner();
+
+public class ResourceManagement{
+	public static void main(String[] args) {
+		double remainingBudget = 0.0;
+		PriorityQueue<Department> pqueue = new PriorityQueue();
+		List<Department> allDepartments = new List();
+		List<String> Purchases = new List();
+		File
+		
+	}
+}
