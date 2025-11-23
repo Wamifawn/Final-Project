039a4f1a43ab3598e5c94989de791bb25541f275
Wamifawn <garrison.lazard@my.utsa.edu>
2025-11-23 17:20:54 -0600

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,52 +1,170 @@
 import java.io.*;
 import java.util.*;
 
-public class Driver {
-
-  public static void main(String[] args) {
-
-    String fileNames[][] = {{
-      "./TestCase1/Department-ComputerScience.txt",
-      "./TestCase1/Department-Mathematics.txt",
-      "./TestCase1/Department-Chemistry.txt",
-      "./TestCase1/Department-PhysicsAndAstronomy.txt"},
-      {
-      "./TestCase1/Department-ComputerScience.txt",
-      "./TestCase1/Department-Mathematics.txt",
-      "./TestCase1/Department-Chemistry.txt",
-      "./TestCase1/Department-PhysicsAndAstronomy.txt"},
-      {
-      "./TestCase2/Department-DeptA.txt",
-      "./TestCase2/Department-DeptB.txt",
-      "./TestCase2/Department-DeptC.txt",
-      "./TestCase2/Department-DeptD.txt",
-      "./TestCase2/Department-DeptE.txt",
-      "./TestCase2/Department-DeptF.txt",
-      "./TestCase2/Department-DeptG.txt"}
-      };
-                                                
-
-    System.out.println(  ); 
-    ResourceManagement.printName( );
-    System.out.println(  );
-
-    //Call student solution for the test files:
-    double budgets[] = {41850.00, 0.01, 27961.10};
-    for( int i=0; i<3; i++ )
-      testFile( fileNames[i], budgets[i] );
-    
-    System.out.println(  ); 
-    ResourceManagement.printName( );
-    System.out.println(  );
-    
+/* ResourceManagement
+ *
+ * Stores the information needed to decide which items to purchase for the given budget and departments
+ */
+public class ResourceManagement
+{
+  private PriorityQueue<Department> departmentPQ = new PriorityQueue<>(); /* priority queue of departments */
+  private Double remainingBudget;                 /* the budget left after purchases are made (should be 0 after the constructor runs) */
+  private Double budget;                          /* the total budget allocated */
+  
+  /* TO BE COMPLETED BY YOU
+   * Fill in your name in the function below
+   */  
+  public static void printName( )
+  {
+    /* TODO : Fill in your name */
+    System.out.println("This solution was completed by:");
+    System.out.println("<student name>");
+    System.out.println("<student name #2 (if no partner write \"N/A\")>");
   }
 
-  public static void testFile( String fileNames[], double budget ) {  
-    String budgetString = String.format("$%.2f", budget );
-    System.out.println("----------------------------TESTING with budget = " + budgetString + "----------------------------");
-    System.out.println("");
+  /* Constructor for a ResourceManagement object
+   * TODO
+   * Simulates the algorithm from the pdf to determine what items are purchased
+   * for the given budget and department item lists.
+   */
+  public ResourceManagement( String fileNames[], Double budget )
+  {
+    /* Create a department for each file listed in fileNames */
+    this.budget = budget;
+    this.remainingBudget = budget;    
+    int i;
+    for ( i = 0; i <= fileNames.length - 1; ++i) {
+    	String fileName = fileNames[i];
+    	Department dept = new Department(fileName);
+    	departmentPQ.add(dept);
+    }
+    
+    /* Simulate the algorithm for picking the items to purchase */
+    /* Be sure to print the items out as you purchase them */
+    /* Here's the part of the code I used for printing prices as items */
+    //String price = String.format("$%.2f", /*Item's price*/ );
+    //System.out.printf("Department of %-30s- %-30s- %30s\n", /*Department's name*/, /*Item's name*/, price );
+    
+    
+  } 
 
-    ResourceManagement rm = new ResourceManagement(fileNames, budget);
-    rm.printSummary();  
+  /* printSummary
+   * TODO
+   * Print a summary of what each department received and did not receive.
+   * Be sure to also print remaining items in each itemsDesired Queue.
+   */      
+  public void printSummary(  ){
+    
+    /* Here's the part of the code I used for printing prices */
+    //String price = String.format("$%.2f", /*Item's price*/ );
+    //System.out.printf("%-30s - %30s\n", /*Item's name*/, price );
+  }   
+}
+
+/* Department
+ *
+ * Stores the information associated with a Department at the university
+ */
+class Department implements Comparable<Department>
+{
+  String name;                /* name of this department */
+  Double priority;            /* total money spent on this department */
+  Queue<Item> itemsDesired;   /* list of items this department wants */
+  Queue<Item> itemsReceived;  /* list of items this department received */
+  Queue<Item> itemsRemoved;   /* list of items that were skipped because they exceeded the remaining budget */
+
+  /* TODO
+   * Constructor to build a Department from the information in the given fileName
+   */
+  public Department( String fileName ){
+    /* Open the fileName, create items based on the contents, and add those items to itemsDesired */
+	  itemsDesired = new LinkedList<>();
+	  itemsReceived = new LinkedList<>();
+	  itemsRemoved = new LinkedList<>();
+	  priority = 0.0;
+	  
+	  File file = new File(fileName);
+	  Scanner input;
+	  try {
+		  input = new Scanner(file);
+		  name = input.next();
+		  while(input.hasNext()) {
+			  String itemName = input.next();
+			  Double itemPrice = input.nextDouble();
+			  Item newItem = new Item(itemName, itemPrice);
+			  itemsDesired.add(newItem);
+		  }
+		  input.close();
+	  }catch(Exception e) {
+		  e.printStackTrace();
+		  System.out.println("The file "+ fileName+" was not found."); return;
+	  }
+	  
+  }
+  
+  /*
+   * Compares the data in the given Department to the data in this Department
+   * Returns -1 if this Department comes first
+   * Returns 0 if these Departments have equal priority
+   * Returns 1 if the given Department comes first
+   *
+   * This function is to ensure the departments are sorted by the priority when put in the priority queue 
+   */
+  public int compareTo( Department dept ){
+    return this.priority.compareTo( dept.priority );
+  }
+
+  public boolean equals( Department dept ){
+    return this.name.compareTo( dept.name ) == 0;
+  }
+
+  @Override 
+  @SuppressWarnings("unchecked") //Suppresses warning for cast
+  public boolean equals(Object aThat) {
+    if (this == aThat) //Shortcut the future comparisons if the locations in memory are the same
+      return true;
+    if (!(aThat instanceof Department))
+      return false;
+    Department that = (Department)aThat;
+    return this.equals( that ); //Use above equals method
+  }
+  
+  @Override
+  public int hashCode() {
+    return name.hashCode(); /* use the hashCode for data stored in this name */
+  }
+
+  /* Debugging tool
+   * Converts this Department to a string
+   */	
+  @Override
+  public String toString() {
+    return "NAME: " + name + "\nPRIORITY: " + priority + "\nDESIRED: " + itemsDesired + "\nRECEIVED " + itemsReceived + "\nREMOVED " + itemsRemoved + "\n";
+  }
+}
+
+/* Item
+ *
+ * Stores the information associated with an Item which is desired by a Department
+ */
+class Item
+{
+  String name;    /* name of this item */
+  Double price;   /* price of this item */
+
+  /*
+   * Constructor to build a Item
+   */
+  public Item( String name, Double price ){
+    this.name = name;
+    this.price = price;
+  }
+
+  /* Debugging tool
+   * Converts this Item to a string
+   */		
+  @Override
+  public String toString() {
+    return "{ " + name + ", " + price + " }";
   }
 }
